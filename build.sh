9dab4b4e935ba607dd2a35942baaf2c7a05d1298
Oliver Tale-Yazdi <oliver@tasty.limo>
2024-09-10 18:55:58 +0200

    Update deps + CI (#12)
    * Update deps
    
    Signed-off-by: Oliver Tale-Yazdi <oliver@tasty.limo>
    
    * Update more
    
    Signed-off-by: Oliver Tale-Yazdi <oliver@tasty.limo>
    
    * CI
    
    Signed-off-by: Oliver Tale-Yazdi <oliver@tasty.limo>
    
    * Build CI
    
    Signed-off-by: Oliver Tale-Yazdi <oliver@tasty.limo>
    
    * Update breaks code...
    
    Signed-off-by: Oliver Tale-Yazdi <oliver@tasty.limo>
    
    * CI
    
    Signed-off-by: Oliver Tale-Yazdi <oliver@tasty.limo>
    
    * set -e
    
    Signed-off-by: Oliver Tale-Yazdi <oliver@tasty.limo>
    
    ---------
    
    Signed-off-by: Oliver Tale-Yazdi <oliver@tasty.limo>

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+set -e
+
 mix deps.get && MIX_ENV=prod mix compile && MIX_ENV=prod mix assets.deploy && MIX_ENV=prod mix release --overwrite && MIX_ENV=prod mix ecto.drop && MIX_ENV=prod mix ecto.setup
