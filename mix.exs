b4ece1ba7edcd80419fa90e4d07c864fbeadb657
Oliver Tale-Yazdi <oliver@tasty.limo>
2024-09-10 18:24:42 +0200

    Update more
    Signed-off-by: Oliver Tale-Yazdi <oliver@tasty.limo>

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule PhoenixApp.MixProject do
   defp deps do
     [
       {:poison, "~> 6.0"},
-      {:bcrypt_elixir, "~> 2.0"},
+      {:bcrypt_elixir, "~> 3.0"},
       {:phoenix, "~> 1.7.14"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
